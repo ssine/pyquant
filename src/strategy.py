e0958d4111fe3d2f7d7989b86d040aa3630f9805
sine <963366202@qq.com>
2020-04-18 01:43:36 +0800

    add base strategy

---
+++ src/strategy.py
@@ -0,0 +1,33 @@
+class BaseStrategy:
+    def __init__(self):
+        pass
+    def on_init(self):
+        pass
+    def on_start(self):
+        pass
+    def on_stop(self):
+        pass
+    def on_tick(self):
+        pass
+    def on_trade(self):
+        pass
+    def on_order(self):
+        pass
+    def buy(self):
+        # buy something
+        pass
+    def sell(self):
+        # sell something
+        pass
+    def short(self):
+        # short something
+        pass
+    def cover(self):
+        # cover something
+        pass
+    def send_order(self):
+        pass
+    def cancel_order(self):
+        pass
+    def cancel_all(self):
+        pass
