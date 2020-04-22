b741ed3371c9c9e805c64be2584ba65996baf66a
sine <963366202@qq.com>
2020-04-22 16:18:16 +0800

    add notebook for testing

---
+++ src/notebook.py
@@ -0,0 +1,25 @@
+#%%
+%load_ext autoreload
+%autoreload 2
+
+#%%
+import sys, os
+from engine import Engine
+from strategy import BaseStrategy
+
+#%%
+eng = Engine()
+eng.load_data('tb', os.path.join(os.path.dirname(__file__), '../data/i2005_Tick.csv'), 'i2005')
+eng.load_data('tb', os.path.join(os.path.dirname(__file__), '../data/i2009_Tick.csv'), 'i2009')
+
+eng.init_exchange()
+
+#%%
+st = BaseStrategy()
+eng.set_strategy(st)
+
+#%%
+tk = eng.step()
+
+
+# %%
